7d6e7409c04a5de00ac48b942467f370f52d073f
Silvia España <silviaespanagil@gmail.com>
2022-02-17 12:49:06 +0100

    Add a new guess view

---
+++ Wordle/Wordle/Models/Views/GuessView.swift
@@ -0,0 +1,35 @@
+//
+//  GuessView.swift
+//  Wordle
+//
+//  Created by Silvia España on 17/2/22.
+//
+
+import SwiftUI
+
+struct GuessView: View {
+    
+    @Binding var guess: Guess
+    
+    var body: some View {
+        
+        HStack(spacing: 3) {
+            
+            ForEach(0...4, id: \.self) { index in
+                
+                Text(guess.guessLetters[index])
+                    .foregroundColor(.primary)
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+                    .background(Color.systemBackground)
+                    .font(.system(size: 3))
+                    
+            }
+        }
+    }
+}
+
+struct GuessView_Previews: PreviewProvider {
+    static var previews: some View {
+        GuessView(guess: .constant(Guess(index: 0)))
+    }
+}
