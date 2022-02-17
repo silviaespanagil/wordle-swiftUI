4988b3bbede5e65563583d3eab99d844c2b6b6c0
Silvia España <silviaespanagil@gmail.com>
2022-02-17 12:52:50 +0100

    Fix Folder location

--- Wordle/Wordle/Views/GuessView.swift
+++ Wordle/Wordle/Views/GuessView.swift
@@ -22,7 +22,7 @@ struct GuessView: View {
                     .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
                     .background(Color.systemBackground)
                     .font(.system(size: 3))
-                    
+                    .border(Color(.secondaryLabel))
             }
         }
     }
