ab1bcebca6a0f7071d036cb306095ceabcb7a589
Silvia España <silviaespanagil@gmail.com>
2022-02-17 12:46:26 +0100

    Change GameView location

---
+++ Wordle/Wordle/Models/Views/GameView.swift
@@ -0,0 +1,57 @@
+//
+//  GameView.swift
+//  Wordle
+//
+//  Created by Silvia España on 17/2/22.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    
+    var body: some View {
+        
+        NavigationView {
+            
+        Text("Hello, world!")
+            .padding()
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "questionmar.circle")
+                    }
+                }
+                ToolbarItem(placement: .principal) {
+                    Text("WORDLE")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundColor(.primary)
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "chart.bar")
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "gearshape.fill")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}
