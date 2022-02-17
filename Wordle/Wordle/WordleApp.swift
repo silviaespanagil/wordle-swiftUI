98be5d1db4b43c38d544c8867a33205ac7d22515
Silvia España <silviaespanagil@gmail.com>
2022-02-17 11:40:17 +0100

    Set up project

---
+++ Wordle/Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Silvia España on 17/2/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
