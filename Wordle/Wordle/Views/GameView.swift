fb0bd8f9a76bd898b507a58584239ecf1ec0e0fb
Silvia España <silviaespanagil@gmail.com>
2022-02-18 13:27:48 +0100

    Update GameView to use new componente GuessView Added keyboard

--- Wordle/Wordle/Views/GameView.swift
+++ Wordle/Wordle/Views/GameView.swift
@@ -9,49 +9,72 @@ import SwiftUI
 
 struct GameView: View {
     
+    @EnvironmentObject var dataModel: WordleDataModel
+    
     var body: some View {
         
         NavigationView {
             
-        Text("Hello, world!")
-            .padding()
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
+            VStack {
+                
+                Spacer()
+                
+                VStack(spacing: 3) {
                     
-                    Button {
+                    GuessView(guess: $dataModel.guesses[0])
+                    GuessView(guess: $dataModel.guesses[1])
+                    GuessView(guess: $dataModel.guesses[2])
+                    GuessView(guess: $dataModel.guesses[3])
+                    GuessView(guess: $dataModel.guesses[4])
+                    GuessView(guess: $dataModel.guesses[5])
+                }
+                .frame(width: Global.boardWidht, height: 6 * Global.boardWidht / 5)
+                
+                Spacer()
+                
+                KeyboardView()
+                    .scaleEffect(Global.keyboardScale)
+                    .padding(.top)
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
                         
-                    } label: {
-                        Image(systemName: "questionmark.circle")
-                    }
-                }
-                ToolbarItem(placement: .principal) {
-                    Text("WORDLE")
-                        .font(.largeTitle)
-                        .fontWeight(.heavy)
-                        .foregroundColor(.primary)
-                }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    HStack {
                         Button {
                             
                         } label: {
-                            Image(systemName: "chart.bar")
+                            Image(systemName: "questionmark.circle")
                         }
-                        Button {
-                            
-                        } label: {
-                            Image(systemName: "gearshape.fill")
+                    }
+                    ToolbarItem(placement: .principal) {
+                        Text("WORDLE")
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(.primary)
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        HStack {
+                            Button {
+                                
+                            } label: {
+                                Image(systemName: "chart.bar")
+                            }
+                            Button {
+                                
+                            } label: {
+                                Image(systemName: "gearshape.fill")
+                            }
                         }
                     }
                 }
             }
         }
+        .navigationViewStyle(.stack)
     }
 }
 
 struct GameView_Previews: PreviewProvider {
     static var previews: some View {
         GameView()
+            .environmentObject(WordleDataModel())
     }
 }
