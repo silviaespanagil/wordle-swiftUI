d17b3d54423cc3b5db91d019c9bf90d00d7e8cba
Silvia España <silviaespanagil@gmail.com>
2022-02-18 13:26:25 +0100

    Added enum to manage dimensions of the game and keyboard

---
+++ Wordle/Wordle/Models/Global.swift
@@ -0,0 +1,48 @@
+//
+//  Global.swift
+//  Wordle
+//
+//  Created by Silvia España on 17/2/22.
+//
+
+import UIKit
+
+enum Global {
+    
+    static var screenWidth: CGFloat {
+        UIScreen.main.bounds.size.width
+    }
+    
+    static var screenHeight: CGFloat {
+        UIScreen.main.bounds.size.height
+    }
+    
+    static var minDimension: CGFloat {
+        min(screenWidth, screenHeight)
+    }
+    
+    static var boardWidht: CGFloat {
+        switch minDimension {
+            
+        case 0...320:
+            return screenWidth - 55
+        case 321...430:
+            return screenWidth - 50
+        case 431...1000:
+            return 350
+        default:
+            return 500
+        }
+    }
+    
+    static var keyboardScale: CGFloat {
+        switch minDimension {
+        case 0...430:
+            return screenWidth / 390
+        case 431...1000:
+            return CGFloat(1.2)
+        default:
+            return CGFloat(1.6)
+        }
+    }
+}
